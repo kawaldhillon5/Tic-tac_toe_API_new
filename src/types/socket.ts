a75638e0811f1d9312b1ebcdae6456f46f3e245b
kawal <kawaldhillon5@gmail.com>
2026-02-07 23:32:26 -0500

    modified

--- src/types/socket.ts
+++ src/types/socket.ts
@@ -5,16 +5,25 @@ import type { Board, Player } from "./game.js";
 export interface ClientToServerEvents {
   join_queue: () => void;
   leave_queue: () => void;
+  join_game : (data:{gameId: string}) => void,
   make_move: (data: { gameId: string; row: number, col: number, player: string }) => void;
 }
 
 // 2. Events the SERVER sends to the CLIENT
 export interface ServerToClientEvents {
   session: (data: { gamerId: string; }) => void;
-  queue_joined: (data: {gameId:string})=>void;
   game_start: (data: { gameId: string; }) => void;
+  game_state: (data: { 
+    gameId: string;
+    board: Board;
+    currentTurn: string | null;
+    mySymbol: 'X' | 'O';
+    opponent: Player | null; // Send full player object (gamerId + mark)
+    status: 'waiting' | 'ongoing' | 'won' | 'draw';
+    winner: string | null;
+  }) => void;
   game_update: (data: { board: Board; currentTurn: string | null }) => void;
-  game_over: (data: { winnerId: string | null;     winningArray: {row:number, col: number}[] | null }) => void;
+  game_over: (data: { winnerId: string | null; winningArray: {row:number, col: number}[] | null }) => void;
   error: (data: { message: string }) => void;
 }
 
