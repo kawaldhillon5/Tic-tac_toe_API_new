7d977212caf34a47d8dc3985b093413cc2b9132c
kawal <kawaldhillon5@gmail.com>
2026-02-23 22:13:56 -0500

    imported env variables

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import { Server } from "socket.io";
 import cors from "cors";
 import type{ TypedServer } from "./types/socket.js";
 import { initializeSockets } from "./sockets/index.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const httpServer = createServer(app);
@@ -12,7 +14,7 @@ app.use(cors({ origin: "*" }));
 
 const io = new Server(httpServer, {
   cors: {
-    origin: "*", // Allow Vite frontend
+    origin: process.env.CLIENT_URL, 
     methods: ["GET", "POST"]
   }
 }) as TypedServer;
